842ef35e6d52241b0cf6708960adb5193a5c1ab5
mattiapennisi <mattiapennisi0@gmail.com>
2025-07-03 10:41:45 +0200

    Add navigation menu to about, contact, and home views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
 </head>
+
 <body>
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('about') }}">About</a></li>
+            <li><a href="{{ route('contact') }}">Contact</a></li>
+        </ul>
+    </nav>
+
     <h1>{{$title}}</h1>
+
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,18 @@ Route::get('/', function () {
     $title = 'Homepage';
 
     return view('home', ['title' => $title]);
-});
+}) -> name('home');
 
 Route::get('/about', function () {
 
     $title = 'About';
 
     return view('about', ['title' => $title]);
-});
+}) -> name('about');
 
 Route::get('/contact', function () {
 
     $title = 'Contact';
 
     return view('contact', ['title' => $title]);
-});
+}) -> name('contact');
