d1f3dddd5e56e0506d7ff47682ced23a1aa0c46f
mattiapennisi <mattiapennisi0@gmail.com>
2025-07-03 10:25:21 +0200

    Add about view and update home route to pass title variable

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+
+    $title = 'Homepage';
+
+    return view('home', ['title' => $title]);
+});
+
+Route::get('/about', function () {
+
+    $title = 'About';
+
+    return view('home', ['title' => $title]);
 });
